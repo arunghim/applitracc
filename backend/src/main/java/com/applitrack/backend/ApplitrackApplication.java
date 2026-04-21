0ff3b9d80769f941ef2a0ec3c2e386f48f4a9fe0
Arun Ghimire <dev.arunghimire@gmail.com>
2026-04-21 14:10:57 -0400

    initial commit: project setup (react + spring)

---
+++ backend/src/main/java/com/applitrack/backend/ApplitrackApplication.java
@@ -0,0 +1,13 @@
+package com.applitrack.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApplitrackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApplitrackApplication.class, args);
+    }
+
+}
